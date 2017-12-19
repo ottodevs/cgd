edbcf85ef08aaba750b5ae14eba69a3c872e4918
apuente93 <apuente@wisc.edu>
2017-12-18 23:39:14 -0700

    Changed data source from bitfinex api to using crypto compare api. This data source contains data for over 50 different cryptocurrencies exchanges. I also removed iota since bitfinex is the only exchange that provides data for it. Finally, I added more exchanges to the table to bring the total of exchanges to 5.

--- app/assets/javascripts/cryptos.js
+++ app/assets/javascripts/cryptos.js
@@ -5,169 +5,62 @@ $(window).on("load resize ", function() {
 
 $(document).ready(function() {
 
-const wss = new WebSocket('wss://api.bitfinex.com/ws/')
+  var currentPrice = {};
+  var socket = io.connect('https://streamer.cryptocompare.com/');
+  //Format: {SubscriptionId}~{ExchangeName}~{FromSymbol}~{ToSymbol}
+  //Use SubscriptionId 0 for TRADE, 2 for CURRENT and 5 for CURRENTAGG
+  //For aggregate quote updates use CCCAGG as market
+  var subscription = ['2~Bitfinex~BTC~USD', '2~Bitfinex~ETH~USD', '2~Bitfinex~BCH~USD',
+  '2~Bitfinex~LTC~USD', '2~Bitfinex~XRP~USD', '2~Bitfinex~XMR~USD',
+  '2~Coinbase~ETH~USD', '2~Coinbase~BTC~USD', '2~Coinbase~LTC~USD', '2~Bitstamp~BTC~USD',
+  '2~Bitstamp~ETH~USD', '2~Bitstamp~BCH~USD', '2~Bitstamp~LTC~USD', '2~XRP~ETH~USD',
+  '2~Poloniex~BTC~USD', '2~Poloniex~ETH~USD', '2~Poloniex~BCH~USD',
+  '2~Poloniex~LTC~USD', '2~Poloniex~XRP~USD', '2~Poloniex~XMR~USD' ,'2~BitTrex~BTC~USD', 
+  '2~BitTrex~ETH~USD', '2~BitTrex~BCH~USD', '2~BitTrex~LTC~USD', '2~BitTrex~XRP~USD', '2~BitTrex~XMR~USD'];
+  socket.emit('SubAdd', { subs: subscription });
+  socket.on("m", function(message) {
+    var messageType = message.substring(0, message.indexOf("~"));
+    var res = {};
+    if (messageType == CCC.STATIC.TYPE.CURRENT) {
+      res = CCC.CURRENT.unpack(message);
+      dataUnpack(res);
+    }
+  });
 
-const btcRequest = {
-  event: 'subscribe', 
-  channel: 'ticker', 
-  symbol: 'tBTCUSD' 
-}
+  var dataUnpack = function(data) {
+    var from = data['FROMSYMBOL'];
+    var to = data['TOSYMBOL'];
+    var market = data['MARKET'];
+    var fsym = CCC.STATIC.CURRENCY.getSymbol(from);
+    var tsym = CCC.STATIC.CURRENCY.getSymbol(to);
+    var pair = from + to;
 
-const ethRequest = {
-  event: 'subscribe', 
-  channel: 'ticker', 
-  symbol: 'tETHUSD' 
-}
-
-const bchRequest = {
-  event: 'subscribe', 
-  channel: 'ticker', 
-  symbol: 'tBCHUSD' 
-}
-
-const xrpRequest = {
-  event: 'subscribe', 
-  channel: 'ticker', 
-  symbol: 'tXRPUSD' 
-}
-
-
-const ltcRequest = {
-  event: 'subscribe', 
-  channel: 'ticker', 
-  symbol: 'tLTCUSD' 
-}
-
-const xmrRequest = {
-  event: 'subscribe', 
-  channel: 'ticker', 
-  symbol: 'tXMRUSD' 
-}
-
-const iotaRequest = {
-  event: 'subscribe', 
-  channel: 'ticker', 
-  symbol: 'tIOTUSD' 
-}
-
-
-const pingTest = {
-  event:"ping",
-   cid: 1234
-}
-
-wss.onopen = () => {
-  wss.send(JSON.stringify(btcRequest));
-  wss.send(JSON.stringify(ethRequest));
-  wss.send(JSON.stringify(bchRequest));
-  wss.send(JSON.stringify(ltcRequest));
-  wss.send(JSON.stringify(xrpRequest));
-  wss.send(JSON.stringify(xmrRequest));
-  wss.send(JSON.stringify(iotaRequest));
-}
-
-var btcId = 0;
-var ethId = 0;
-var bchId = 0;
-var ltcId = 0;
-var xrpId = 0;
-var xmrId = 0;
-var iotaId = 0;
-
-var btcPrice = document.getElementById("btcPrice");
-var ethPrice = document.getElementById("ethPrice");
-var bchPrice = document.getElementById("bchPrice");
-var ltcPrice = document.getElementById("ltcPrice");
-var xrpPrice = document.getElementById("xrpPrice");
-var xmrPrice = document.getElementById("xmrPrice");
-var iotaPrice = document.getElementById("iotaPrice");
-
-var btcPercentChange = document.getElementById("btcPercentChange");
-var ethPercentChange = document.getElementById("ethPercentChange");
-var bchPercentChange = document.getElementById("bchPercentChange");
-var ltcPercentChange = document.getElementById("ltcPercentChange");
-var xrpPercentChange = document.getElementById("xrpPercentChange");
-var xmrPercentChange = document.getElementById("xmrPercentChange");
-var iotaPercentChange = document.getElementById("iotaPercentChange");
-
-wss.onmessage = function(response) {
-    console.log(response);
-    var jsonResponse = JSON.parse(response.data);
-    var chanId = "";
-    var pair = "";
-
-    if (jsonResponse['event'] === "subscribed") {
-      chanId = jsonResponse['chanId'];
-      pair = jsonResponse['pair'];
-
-      if (pair === "BTCUSD") {
-        btcId = chanId;
-      }
-      else if (pair === "ETHUSD") {
-        ethId = chanId;
-      }
-      else  if (pair === "BCHUSD") {
-        bchId = chanId;
-      }
-      else if (pair === "LTCUSD") {
-        ltcId = chanId;
-      }
-      else  if (pair === "XRPUSD") {
-        xrpId = chanId;
-      }
-      else  if (pair === "XMRUSD") {
-        xmrId = chanId;
-      }
-      else  if (pair === "IOTUSD") {
-        iotaId = chanId;
-      }
-      else  {
-        ;
-      }
+    if (!currentPrice.hasOwnProperty(pair)) {
+      currentPrice[pair] = {};
     }
 
-    if (jsonResponse[0] == btcId) {
-      if (typeof jsonResponse[7] != 'undefined') {
-        btcPrice.innerText = accounting.formatMoney(jsonResponse[7]);
-        btcPercentChange.innerText = (jsonResponse[6] * 100).toFixed(2) + '%';
-      }
+    for (var key in data) {
+      currentPrice[pair][key] = data[key];
     }
-    else if (jsonResponse[0] == ethId) {
-      if (typeof jsonResponse[7] != 'undefined') {
-        ethPrice.innerText = accounting.formatMoney(jsonResponse[7]);
-        ethPercentChange.innerText = (jsonResponse[6] * 100).toFixed(2) + '%';
-      }
-    }
-    else if (jsonResponse[0] == bchId) {
-      if (typeof jsonResponse[7] != 'undefined') {
-        bchPrice.innerText = accounting.formatMoney(jsonResponse[7]);
-        bchPercentChange.innerText = (jsonResponse[6] * 100).toFixed(2) + '%';
-      }
-    }
-    else if (jsonResponse[0] == ltcId) {
-      if (typeof jsonResponse[7] != 'undefined') {
-        ltcPrice.innerText = accounting.formatMoney(jsonResponse[7]);
-        ltcPercentChange.innerText = (jsonResponse[6] * 100).toFixed(2) + '%';
-      }
-    }
-    else if (jsonResponse[0] == xrpId) {
-      if (typeof jsonResponse[7] != 'undefined') {
-        xrpPrice.innerText = accounting.formatMoney(jsonResponse[7]);
-        xrpPercentChange.innerText = (jsonResponse[6] * 100).toFixed(2) + '%';
-      }
-    }
-    else if (jsonResponse[0] == xmrId) {
-      if (typeof jsonResponse[7] != 'undefined') {
-        xmrPrice.innerText = accounting.formatMoney(jsonResponse[7]);
-        xmrPercentChange.innerText = (jsonResponse[6] * 100).toFixed(2) + '%';
-      }
-    }
-    else if (jsonResponse[0] == iotaId) {
-      if (typeof jsonResponse[7] != 'undefined') {
-        iotaPrice.innerText = accounting.formatMoney(jsonResponse[7]);
-        iotaPercentChange.innerText = (jsonResponse[6] * 100).toFixed(2) + '%';
-      }
-    }
-}
 
+    if (currentPrice[pair]['LASTTRADEID']) {
+      currentPrice[pair]['LASTTRADEID'] = parseInt(currentPrice[pair]['LASTTRADEID']).toFixed(0);
+    }
+    currentPrice[pair]['CHANGE24HOUR'] = CCC.convertValueToDisplay(tsym, (currentPrice[pair]['PRICE'] - currentPrice[pair]['OPEN24HOUR']));
+    currentPrice[pair]['CHANGE24HOURPCT'] = ((currentPrice[pair]['PRICE'] - currentPrice[pair]['OPEN24HOUR']) / currentPrice[pair]['OPEN24HOUR'] * 100).toFixed(2) + "%";;
+    displayData(currentPrice[pair], from, tsym, fsym, market);
+  };
+
+  var displayData = function(current, from, tsym, fsym, market) {
+    console.log(current);
+    var priceDirection = current.FLAGS;
+    for (var key in current) {
+      if (key == 'CHANGE24HOURPCT') {
+        $('#' + key + '_' + market + '_' + from).text(current[key]);
+      }
+      else if (key == 'PRICE') {
+        $('#' + key + '_' + market + '_' + from).text(accounting.formatMoney(current[key]));
+      }
+    }
+  };
 });
