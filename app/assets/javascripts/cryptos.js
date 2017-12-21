3918067e94c197a32a356cdbc6d82e312868abfc
apuente93 <apuente@wisc.edu>
2017-12-20 17:32:53 -0700

    changed design a little bit

--- app/assets/javascripts/cryptos.js
+++ app/assets/javascripts/cryptos.js
@@ -1,8 +1,3 @@
-$(window).on("load resize ", function() {
-  var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
-  $('.tbl-header').css({'padding-right':scrollWidth});
-}).resize();
-
 $(document).ready(function() {
 
   var currentPrice = {};
@@ -11,9 +6,9 @@ $(document).ready(function() {
   //Use SubscriptionId 0 for TRADE, 2 for CURRENT and 5 for CURRENTAGG
   //For aggregate quote updates use CCCAGG as market
   var subscription = ['2~Bitfinex~BTC~USD', '2~Bitfinex~ETH~USD', '2~Bitfinex~BCH~USD',
-  '2~Bitfinex~LTC~USD', '2~Bitfinex~XRP~USD', '2~Bitfinex~XMR~USD',
+  '2~Bitfinex~LTC~USD', '2~Bitfinex~XRP~USD', '2~Bitfinex~XMR~USD', '2~Coinbase~BCH~USD',
   '2~Coinbase~ETH~USD', '2~Coinbase~BTC~USD', '2~Coinbase~LTC~USD', '2~Bitstamp~BTC~USD',
-  '2~Bitstamp~ETH~USD', '2~Bitstamp~BCH~USD', '2~Bitstamp~LTC~USD', '2~XRP~ETH~USD',
+  '2~Bitstamp~ETH~USD', '2~Bitstamp~BCH~USD', '2~Bitstamp~LTC~USD', '2~Bitstamp~XRP~USD',
   '2~Poloniex~BTC~USD', '2~Poloniex~ETH~USD', '2~Poloniex~BCH~USD',
   '2~Poloniex~LTC~USD', '2~Poloniex~XRP~USD', '2~Poloniex~XMR~USD' ,'2~BitTrex~BTC~USD', 
   '2~BitTrex~ETH~USD', '2~BitTrex~BCH~USD', '2~BitTrex~LTC~USD', '2~BitTrex~XRP~USD', '2~BitTrex~XMR~USD'];
@@ -47,20 +42,37 @@ $(document).ready(function() {
       currentPrice[pair]['LASTTRADEID'] = parseInt(currentPrice[pair]['LASTTRADEID']).toFixed(0);
     }
     currentPrice[pair]['CHANGE24HOUR'] = CCC.convertValueToDisplay(tsym, (currentPrice[pair]['PRICE'] - currentPrice[pair]['OPEN24HOUR']));
-    currentPrice[pair]['CHANGE24HOURPCT'] = ((currentPrice[pair]['PRICE'] - currentPrice[pair]['OPEN24HOUR']) / currentPrice[pair]['OPEN24HOUR'] * 100).toFixed(2) + "%";;
+    currentPrice[pair]['CHANGE24HOURPCT'] = ((currentPrice[pair]['PRICE'] - currentPrice[pair]['OPEN24HOUR']) / currentPrice[pair]['OPEN24HOUR'] * 100).toFixed(2) + "%";
     displayData(currentPrice[pair], from, tsym, fsym, market);
+    flashChange();
   };
 
   var displayData = function(current, from, tsym, fsym, market) {
-    console.log(current);
     var priceDirection = current.FLAGS;
     for (var key in current) {
       if (key == 'CHANGE24HOURPCT') {
-        $('#' + key + '_' + market + '_' + from).text(current[key]);
+        $('#' + key + '_' + market + '_' + from).text('(' + current[key] + ')');
       }
       else if (key == 'PRICE') {
         $('#' + key + '_' + market + '_' + from).text(accounting.formatMoney(current[key]));
       }
     }
+
+    if (current['PRICE'] > current['OPEN24HOUR']) {
+      $('#CHANGE24HOURPCT_' + market + '_' + from).removeClass();
+      $('#CHANGE24HOURPCT_' + market + '_' + from).addClass("up");
+    }
+    else if (current['PRICE'] < current['OPEN24HOUR']) {
+      $('#CHANGE24HOURPCT_' + market + '_' + from).removeClass();
+      $('#CHANGE24HOURPCT_' + market + '_' + from).addClass("down");
+    } 
   };
+
+  var flashChange = function() {
+      $("#Coinbase_BTC").removeClass("demo");
+        setTimeout(function() {
+        $("#Coinbase_BTC").addClass("demo");
+    }, 1);
+  };
+
 });
